bc4dd8a4178d5002f085ad7513e712f0e04047c3
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-16 14:28:48 +0200

    add to the performance data spec the test for creating a collection of objects for the user that makes the request

--- cooper_test_rails/spec/requests/api/v1/performance_data_spec.rb
+++ cooper_test_rails/spec/requests/api/v1/performance_data_spec.rb
@@ -12,4 +12,17 @@ describe 'Performance Data' do
       expect(entry.data).to eq({'message' => 'Average'})
     end
   end
+
+  describe 'GET /api/v1/data/' do
+    before do
+      5.times { user.performance_data.create(data: {message: 'Average'}) }
+    end
+
+    it 'returns a collection' do
+      get '/api/v1/data/', {}, headers.merge!(credentials)
+      expect(response_json['entries'].count).to eq 5
+    end
+  end
+
+
 end
