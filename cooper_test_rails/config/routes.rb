48fd7739e0a26c8496a1ffb1b5e9489fbb0433a5
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-08-16 14:31:38 +0200

    adds data controller route for 'get' performance data in the routes.rb

--- cooper_test_rails/config/routes.rb
+++ cooper_test_rails/config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     post 'data', controller: :performance_data, action: :create, as: :create
+    get 'data', controller: :performance_data, action: :index, as: :index
   end
 end
